9024d2e8784ee344cefda4a2c98852e9cc24c7ca
YaaraPer <yaaraperlmuter@gmail.com>
2026-01-22 17:26:23 +0200

    ביצעתי ייבוא של המודול random' כתבתי פונקציה שדואגת שהאותיות תהיינה אחידות (אותיות גדולות) והופכת את הרצף מ DNA ל RNA.

---
+++ scripts/code.py
@@ -0,0 +1,17 @@
+import random
+
+def DNA_RNA_Cod(seq):
+  '''
+  הפונקציה דואגת שהאותיות תהיינה אחידות (אותיות גדולות) והופכת את רצף ה- DNA המקודד לרצף RNA.
+  מקבלת: seq.
+  מחזירה: RNA_seq.
+  '''
+  RNA_seq=""
+
+
+  if seq[0] != ">":
+    seq = seq.upper()
+    RNA_seq = seq.replace("T","U")
+ 
+  return RNA_seq
+#------------------------------------------------
