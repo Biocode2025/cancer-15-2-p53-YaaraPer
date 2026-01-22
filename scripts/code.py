db92ec58a48e3d7eba69db7d8d198b5ce668b008
YaaraPer <yaaraperlmuter@gmail.com>
2026-01-22 18:05:00 +0200

    כתבתי פונקציה שבוחרת מיקום אקראי ברצף שמקבלת ומחליפה במיקום זה את הנוקלאוטיד באופן רנדומלי ל- A/C/G/T.

--- scripts/code.py
+++ scripts/code.py
@@ -95,3 +95,24 @@ def Delete_DNA(seq):
 
   return change_genome
 #------------------------------------------------
+
+
+def Mutate_DNA(seq):
+  '''
+  הפונקציה בוחרת מיקום אקראי ברצף שמקבלת ומחליפה במיקום זה את הנוקלאוטיד באופן רנדומלי ל- A/C/G/T.
+  מקבלת: seq.
+  מחזירה:change_genome
+  '''
+  nucleotide_list = ['T','G','C','A']
+ 
+  rand_nucleotide = random.choice(nucleotide_list)
+  rand_num = random.randrange(0,len(seq))
+ 
+  if seq[rand_num] != rand_nucleotide:
+    change_genome = seq[0:rand_num]+ rand_nucleotide + seq[(rand_num+1):]
+ 
+  else:
+    nucleotide_list.remove(rand_nucleotide)
+    rand_nucleotide = random.choice(nucleotide_list)
+    change_genome = seq[0:rand_num]+ rand_nucleotide + seq[(rand_num+1):]
+  return change_genome
